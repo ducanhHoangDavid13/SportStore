473ae9ac570120e3b3a2171ad06dfe544303641d
ducanhHoangDavid13 <ducdavidwork2024@gmail.com>
2025-10-14 20:19:03 +0700

    Lan dau

---
+++ src/main/java/sd_04/datn_fstore/DatnFstoreApplication.java
@@ -0,0 +1,13 @@
+package sd_04.datn_fstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatnFstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatnFstoreApplication.class, args);
+    }
+
+}
